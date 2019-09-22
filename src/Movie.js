0bfcc310481406ce0ab3977f85c54dbe24d923c2
park kyung suk <boardguy1024@gmail.com>
2019-09-22 19:25:25 +0900

    add genres

--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, year, rating, title, summary, coverImage }) {
+function Movie({ id, year, rating, title, summary, coverImage, genres }) {
   return (
-    <div>
-      <h3>id: {id}</h3>
-      <h3>year: {year}</h3>
-      <h3>rating: {rating}</h3>
-      <h3>title: {title}</h3>
-      <h3>summary: {summary}</h3>
-      <img src={coverImage} alt={title}></img>
+    <div className="movie">
+      <h3 className="movie__year">year: {year}</h3>
+      <ul className="movie__genres">
+        {genres.map((genre, index) => (
+          <li key={index} className="movie__genre">
+            {genre}
+          </li>
+        ))}
+      </ul>
+      <h3 className="movie__rating">rating: {rating}</h3>
+      <h3 className="movie__title">title: {title}</h3>
+      <p className="movie__summary">summary: {summary}</p>
+      <img
+        className="movie__image"
+        src={coverImage}
+        alt={title}
+        title={title}
+      ></img>
       ===========================
     </div>
   );
@@ -21,7 +33,8 @@ Movie.prototype = {
   rating: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  coverImage: PropTypes.string.isRequired
+  coverImage: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
